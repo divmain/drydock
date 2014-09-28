283385475bd459fa050e0ac321d1806ac0438695
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-27 18:27:36 -0700

    no need to chain ajax GETs

--- frontend/main.js
+++ frontend/main.js
@@ -3,11 +3,8 @@ define([
   "frontend/util/ajax",
   "frontend/views/control-panel/v-control-panel"
 ], function (_, ajax, ControlPanel) {
-  var routes;
-
   ajax.get("/surrogate/api/routes").then(function (response) {
-    routes = JSON.parse(response.data);
-  }).then(function () {
+    var routes = JSON.parse(response.data);
     (new ControlPanel({
       data: {
         routes: routes,
