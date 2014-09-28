1357304bb0e2e29508f79249fa9ac9420ca59904
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-28 15:51:12 -0700

    set initial state for rote expanded

--- frontend/main.js
+++ frontend/main.js
@@ -4,14 +4,17 @@ define([
   "frontend/views/control-panel/v-control-panel"
 ], function (_, ajax, ControlPanel) {
   ajax.get("/surrogate/api/routes").then(function (response) {
-    var
-      routes = JSON.parse(response.data),
-      controlPanel = new ControlPanel({
-        data: {
-          routes: routes,
-          delay: 0
-        }
-      });
+    var controlPanel,
+      routes = JSON.parse(response.data);
+
+    _.each(routes, function (route) { route.expanded = false; });
+
+    controlPanel = new ControlPanel({
+      data: {
+        routes: routes,
+        delay: 0
+      }
+    });
 
     controlPanel.$mount("#control-panel");
   });
