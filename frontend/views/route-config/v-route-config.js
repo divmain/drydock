4c204110b4fafe5a419c5b874378a794e5f659a3
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-29 16:59:49 -0700

    v-route-config.js --> CJS

--- frontend/views/route-config/v-route-config.js
+++ frontend/views/route-config/v-route-config.js
@@ -1,71 +1,69 @@
-define([
-  "lodash",
-  "vue",
-  "frontend/util/ajax",
-  "frontend/helpers/watch-hash",
-  "./v-route-config.tmpl",
-  "./v-route-config.styl"
-], function (_, Vue, ajax, watchHash, tmpl) {
+var
+  ajax = require("frontend/util/ajax"),
+  watchHash = require("frontend/helpers/watch-hash"),
+  tmpl = require("./v-route-config.tmpl"),
+  _ = require("lodash"),
+  Vue = require("vue");
 
-  return Vue.extend({
-    template: tmpl,
+require("./v-route-config.styl");
 
-    mixins: [
-      watchHash
-    ],
+module.exports = Vue.extend({
+  template: tmpl,
 
-    computed: {
-      selectedHandlerObj: function () {
-        return _.find(this.handlers, { name: this.selectedHandler });
-      }
+  mixins: [
+    watchHash
+  ],
+
+  computed: {
+    selectedHandlerObj: function () {
+      return _.find(this.handlers, { name: this.selectedHandler });
+    }
+  },
+
+  watch: {
+    "selectedHandler": function (newValue) {
+      ajax.put("/surrogate/api/route/handler", {
+        data: {
+          method: this.$data.method,
+          path: this.$data.path,
+          selectedHandler: newValue
+        }
+      });
     },
-
-    watch: {
-      "selectedHandler": function (newValue) {
-        ajax.put("/surrogate/api/route/handler", {
+    "selectedHandlerObj.selectedOption": function (newSelection) {
+      if (newSelection) {
+        ajax.put("/surrogate/api/route/selected-option", {
           data: {
             method: this.$data.method,
             path: this.$data.path,
-            selectedHandler: newValue
+            handler: this.$data.selectedHandler,
+            selectedOption: newSelection
           }
         });
-      },
-      "selectedHandlerObj.selectedOption": function (newSelection) {
-        if (newSelection) {
-          ajax.put("/surrogate/api/route/selected-option", {
-            data: {
-              method: this.$data.method,
-              path: this.$data.path,
-              handler: this.$data.selectedHandler,
-              selectedOption: newSelection
-            }
-          });
-        }
-      },
-      "selectedHandlerObj.selectedOptions": function (newSelections) {
-        if (newSelections) {
-          ajax.put("/surrogate/api/route/selected-options", {
-            data: {
-              method: this.$data.method,
-              path: this.$data.path,
-              handler: this.$data.selectedHandler,
-              selectedOptions: newSelections
-            }
-          });
-        }
       }
     },
-
-    methods: {
-      updateSelectedOptions: function (handler, newSelection) {
-        handler.selectedOptions = _.filter(handler.options, function (option) {
-          return _.contains(handler.selectedOptions, option) || option === newSelection;
+    "selectedHandlerObj.selectedOptions": function (newSelections) {
+      if (newSelections) {
+        ajax.put("/surrogate/api/route/selected-options", {
+          data: {
+            method: this.$data.method,
+            path: this.$data.path,
+            handler: this.$data.selectedHandler,
+            selectedOptions: newSelections
+          }
         });
-      },
-      isSelected: function (handler, option) {
-        return _.contains(handler.selectedOptions, option);
       }
     }
-  });
+  },
 
+  methods: {
+    updateSelectedOptions: function (handler, newSelection) {
+      handler.selectedOptions = _.filter(handler.options, function (option) {
+        return _.contains(handler.selectedOptions, option) || option === newSelection;
+      });
+    },
+    isSelected: function (handler, option) {
+      return _.contains(handler.selectedOptions, option);
+    }
+  }
 });
