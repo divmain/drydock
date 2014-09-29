1b57569dadba68993bddf9859996357ce53a3884
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-28 22:50:09 -0700

    reorganize control panel view js and auto-close expanded route config when another opens

--- frontend/views/control-panel/v-control-panel.js
+++ frontend/views/control-panel/v-control-panel.js
@@ -2,20 +2,61 @@ define([
   "lodash",
   "vue",
   "frontend/util/ajax",
+  "frontend/helpers/watch-hash",
   "frontend/views/route-config/v-route-config",
   "./v-control-panel.tmpl",
   "./v-control-panel.styl"
-], function (_, Vue, ajax, RouteConfig, tmpl) {
+], function (_, Vue, ajax, watchHash, RouteConfig, tmpl) {
 
   return Vue.extend({
     template: tmpl,
 
-    created: function () {
-      this.$watch("delay", _.debounce(function (newValue) {
+    components: {
+      "route-config": RouteConfig
+    },
+
+    mixins: [
+      watchHash
+    ],
+
+    watch: {
+      "delay": _.debounce(function (newValue) {
         ajax.put("/surrogate/api/delay", {
           data: { delay: newValue }
         });
-      }, 300));
+      }, 300)
+    },
+
+    computed: {
+      // range 0 - 10
+      delayWidgetPosition: {
+        get: function () {
+          var number;
+          if (this.delay === 0) {
+            return 0;
+          } else if (this.delay === null  ) {
+            return 10;
+          }
+          number = Math.log(this.delay / 10) / Math.log(2);
+          return parseFloat(number.toPrecision(2));
+        },
+        set: function (newValue) {
+          newValue = parseFloat(newValue);
+          if (newValue === 0) {
+            this.delay = 0;
+          } else if (newValue === 10) {
+            this.delay = null;
+          } else {
+            this.delay = Math.pow(2, newValue) * 10;
+          }
+        }
+      },
+
+      delayDisplay: function () {
+        return this.delay === "forever" ?
+          "HANG" :
+          this.delay.toString() + " ms";
+      }
     },
 
     methods: {
@@ -25,43 +66,12 @@ define([
         });
       },
       toggleRouteConfig: function (route) {
-        route.expanded = !route.expanded;
+        var alreadyExpanded = route.expanded;
+        _.each(this.routes, function (route) {
+          route.expanded = false;
+        });
+        route.expanded = !alreadyExpanded;
       }
-    },
-
-    computed: {
-      // range 0 - 10
-      delayWidgetPosition: {
-        $get: function () {
-          var number;
-          if (this.delay === 0) {
-            return 0;
-          } else if (this.delay === "forever") {
-            return 10;
-          }
-          number = Math.log(this.delay / 10) / Math.log(2);
-          return parseFloat(number.toPrecision(2));
-        },
-        $set: function (newValue) {
-          newValue = parseFloat(newValue);
-          if (newValue === 0) {
-            this.delay = 0;
-          } else if (newValue === 10) {
-            this.delay = "forever";
-          } else {
-            this.delay = Math.pow(2, newValue) * 10;
-          }
-        }
-      },
-      delayDisplay: function () {
-        return this.delay === "forever" ?
-          "HANG" :
-          this.delay.toString() + " ms";
-      }
-    },
-
-    components: {
-      "route-config": RouteConfig
     }
   });
 
