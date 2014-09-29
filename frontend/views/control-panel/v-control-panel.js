5fd0199dfaa266b6c7536cbac4208cc2ff16cbb7
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-29 16:58:21 -0700

    v-control-panel.js --> CJS

--- frontend/views/control-panel/v-control-panel.js
+++ frontend/views/control-panel/v-control-panel.js
@@ -1,84 +1,82 @@
-define([
-  "lodash",
-  "vue",
-  "frontend/util/ajax",
-  "frontend/helpers/watch-hash",
-  "frontend/views/route-config/v-route-config",
-  "./v-control-panel.tmpl",
-  "./v-control-panel.styl"
-], function (_, Vue, ajax, watchHash, RouteConfig, tmpl) {
+var
+  ajax = require("frontend/util/ajax"),
+  RouteConfig = require("frontend/views/route-config/v-route-config"),
+  watchHash = require("frontend/helpers/watch-hash"),
+  tmpl = require("./v-control-panel.tmpl"),
+  _ = require("lodash"),
+  Vue = require("vue");
 
-  return Vue.extend({
-    template: tmpl,
+require("./v-control-panel.styl");
 
-    components: {
-      "route-config": RouteConfig
-    },
+module.exports = Vue.extend({
+  template: tmpl,
 
-    mixins: [
-      watchHash
-    ],
+  components: {
+    "route-config": RouteConfig
+  },
 
-    watch: {
-      "delay": _.debounce(function (newValue) {
-        ajax.put("/surrogate/api/delay", {
-          data: { delay: newValue }
-        });
-      }, 300)
-    },
+  mixins: [
+    watchHash
+  ],
 
-    computed: {
-      // range 0 - 10
-      delayWidgetPosition: {
-        get: function () {
-          var number;
-          if (this.delay === 0) {
-            return 0;
-          } else if (this.delay === null  ) {
-            return 10;
-          }
-          number = Math.log(this.delay / 10) / Math.log(2);
-          return parseFloat(number.toPrecision(2));
-        },
-        set: function (newValue) {
-          newValue = parseFloat(newValue);
-          if (newValue === 0) {
-            this.delay = 0;
-          } else if (newValue === 10) {
-            this.delay = null;
-          } else {
-            this.delay = Math.pow(2, newValue) * 10;
-          }
+  watch: {
+    "delay": _.debounce(function (newValue) {
+      ajax.put("/surrogate/api/delay", {
+        data: { delay: newValue }
+      });
+    }, 300)
+  },
+
+  computed: {
+    // range 0 - 10
+    delayWidgetPosition: {
+      get: function () {
+        var number;
+        if (this.delay === 0) {
+          return 0;
+        } else if (this.delay === null  ) {
+          return 10;
         }
+        number = Math.log(this.delay / 10) / Math.log(2);
+        return parseFloat(number.toPrecision(2));
       },
-
-      delayDisplay: function () {
-        return this.delay === "forever" ?
-          "HANG" :
-          this.delay.toString() + " ms";
+      set: function (newValue) {
+        newValue = parseFloat(newValue);
+        if (newValue === 0) {
+          this.delay = 0;
+        } else if (newValue === 10) {
+          this.delay = null;
+        } else {
+          this.delay = Math.pow(2, newValue) * 10;
+        }
       }
     },
 
-    methods: {
-      resetSurrogate: function () {
-        var self = this;
-        return ajax.post("/surrogate/api/reset").then(function () {
-          self._getDocument().location.reload();
-        });
-      },
-
-      toggleRouteConfig: function (route) {
-        var alreadyExpanded = route.expanded;
-        _.each(this.routes, function (route) {
-          route.expanded = false;
-        });
-        route.expanded = !alreadyExpanded;
-      },
-
-      _getDocument: function () {
-        return document;
-      }
+    delayDisplay: function () {
+      return this.delay === "forever" ?
+        "HANG" :
+        this.delay.toString() + " ms";
     }
-  });
+  },
 
+  methods: {
+    resetSurrogate: function () {
+      var self = this;
+      return ajax.post("/surrogate/api/reset").then(function () {
+        self._getDocument().location.reload();
+      });
+    },
+
+    toggleRouteConfig: function (route) {
+      var alreadyExpanded = route.expanded;
+      _.each(this.routes, function (route) {
+        route.expanded = false;
+      });
+      route.expanded = !alreadyExpanded;
+    },
+
+    _getDocument: function () {
+      return document;
+    }
+  }
 });
