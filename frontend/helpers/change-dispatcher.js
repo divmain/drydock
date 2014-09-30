e9dae8469409b7266bd67c339b73dde21a7b6230
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-29 17:03:42 -0700

    change-dispatcher.js --> CJS

--- frontend/helpers/change-dispatcher.js
+++ frontend/helpers/change-dispatcher.js
@@ -1,20 +1,18 @@
-define([
-  "lodash"
-], function (_) {
-  return {
-    compiled: function () {
-      _.each(this.$options.dispatchers, function (targets, keypath) {
-        this.$watch(keypath, function () {
-          var payload = _.chain(targets)
-            .map(function (dataProperty, payloadProperty) {
-              return [payloadProperty, this.$data[dataProperty]];
-            }, this)
-            .object()
-            .value();
-          console.log("change:" + keypath, payload);
-          this.$dispatch("change:" + keypath, payload);
-        });
-      }, this);
-    }
-  };
-});
+var
+  _ = require("lodash");
+
+module.exports = {
+  compiled: function () {
+    _.each(this.$options.dispatchers, function (targets, keypath) {
+      this.$watch(keypath, function () {
+        var payload = _.chain(targets)
+          .map(function (dataProperty, payloadProperty) {
+            return [payloadProperty, this.$data[dataProperty]];
+          }, this)
+          .object()
+          .value();
+        this.$dispatch("change:" + keypath, payload);
+      });
+    }, this);
+  }
+};
