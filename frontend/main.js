8bb5d1fbbb1410e53d4b791ebca978d8977232df
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-29 17:02:02 -0700

    main.js --> CJS

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,20 @@
-define([
-  "lodash",
-  "frontend/util/ajax",
-  "frontend/views/control-panel/v-control-panel"
-], function (_, ajax, ControlPanel) {
-  ajax.get("/surrogate/api/routes").then(function (response) {
-    var controlPanel,
-      routes = JSON.parse(response.data);
+var
+  ajax = require("frontend/util/ajax"),
+  _ = require("lodash"),
+  ControlPanel = require("frontend/views/control-panel-v-control-panel");
 
-    _.each(routes, function (route) { route.expanded = false; });
+ajax.get("/surrogate/api/routes").then(function (response) {
+  var controlPanel,
+    routes = JSON.parse(response.data);
 
-    controlPanel = new ControlPanel({
-      data: {
-        routes: routes,
-        delay: 0
-      }
-    });
+  _.each(routes, function (route) { route.expanded = false; });
 
-    controlPanel.$mount("#control-panel");
+  controlPanel = new ControlPanel({
+    data: {
+      routes: routes,
+      delay: 0
+    }
   });
+
+  controlPanel.$mount("#control-panel");
 });
