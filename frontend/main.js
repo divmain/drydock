19b62058d4349db284742d5124005e8c006b9d43
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-29 21:41:20 -0700

    typo

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,7 @@
 var
   ajax = require("frontend/util/ajax"),
   _ = require("lodash"),
-  ControlPanel = require("frontend/views/control-panel-v-control-panel");
+  ControlPanel = require("frontend/views/control-panel/v-control-panel");
 
 ajax.get("/surrogate/api/routes").then(function (response) {
   var controlPanel,
