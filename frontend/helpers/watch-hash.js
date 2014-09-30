5a52ac8faa8208ec4cf899d27943596f03f1a09f
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-29 17:03:12 -0700

    watch-hash --> CJS

--- frontend/helpers/watch-hash.js
+++ frontend/helpers/watch-hash.js
@@ -1,11 +1,10 @@
-define([
-  "lodash"
-], function (_) {
-  return {
-    compiled: function () {
-      _.each(this.$options.watch, function (handler, keypath) {
-        this.$watch(keypath, handler);
-      }, this);
-    }
-  };
-});
+var
+  _ = require("lodash");
+
+module.exports = {
+  compiled: function () {
+    _.each(this.$options.watch, function (handler, keypath) {
+      this.$watch(keypath, handler);
+    }, this);
+  }
+};
