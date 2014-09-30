3e9514d1dc54e78540d67dff87988f412bf829dd
Dale Bustad <dale.bustad@formidablelabs.com>
2014-09-29 17:02:42 -0700

    ajax.js --> CJS

--- frontend/util/ajax.js
+++ frontend/util/ajax.js
@@ -1,95 +1,89 @@
-define([
-  "lodash",
-  "bluebird"
-], function (
-  _,
-  Promise
-) {
+var
+  _ = require("lodash"),
+  Promise = require("bluebird");
 
   var assertSuccess, parseHeaders, buildResolved, buildRejected, request,
     justWhitespace = /^\s*$/;
 
-  assertSuccess = function (resolvedXhr) {
-    if (resolvedXhr.status > 299 || resolvedXhr.status < 200) {
-      throw resolvedXhr;
-    }
-    return resolvedXhr;
+assertSuccess = function (resolvedXhr) {
+  if (resolvedXhr.status > 299 || resolvedXhr.status < 200) {
+    throw resolvedXhr;
+  }
+  return resolvedXhr;
+};
+
+parseHeaders = function (headerText) {
+  return _.chain(headerText.split("\n"))
+    .filter(function (headerLine) {
+      return justWhitespace.test(headerLine);
+    })
+    .map(function (headerLine) {
+      var splitIndex = headerLine.indexOf(":");
+      return [
+        headerLine.slice(0, splitIndex).trim(),
+        headerLine.slice(splitIndex + 1).trim()
+      ];
+    })
+    .object()
+    .value();
+};
+
+buildResolved = function (resolve) {
+  return function (xhr) {
+    resolve({
+      data: xhr.target.response,
+      headers: parseHeaders(xhr.target.getAllResponseHeaders()),
+      status: xhr.target.status,
+      statusText: xhr.target.statusText
+    });
   };
+};
 
-  parseHeaders = function (headerText) {
-    return _.chain(headerText.split("\n"))
-      .filter(function (headerLine) {
-        return justWhitespace.test(headerLine);
-      })
-      .map(function (headerLine) {
-        var splitIndex = headerLine.indexOf(":");
-        return [
-          headerLine.slice(0, splitIndex).trim(),
-          headerLine.slice(splitIndex + 1).trim()
-        ];
-      })
-      .object()
-      .value();
+buildRejected = function (reject) {
+  return function (xhr) {
+    reject(xhr);
   };
+};
 
-  buildResolved = function (resolve) {
-    return function (xhr) {
-      resolve({
-        data: xhr.target.response,
-        headers: parseHeaders(xhr.target.getAllResponseHeaders()),
-        status: xhr.target.status,
-        statusText: xhr.target.statusText
-      });
-    };
-  };
+request = function (method, url, options) {
+  var data, xhr, username, password;
 
-  buildRejected = function (reject) {
-    return function (xhr) {
-      reject(xhr);
-    };
-  };
+  method = (method || "GET").toUpperCase();
+  options = options || {};
+  options.contentType = options.contentType || "application/json; charset=utf-8";
+  username = _.isString(options.username) && options.username || "";
+  password = _.isString(options.password) && options.password || "";
 
-  request = function (method, url, options) {
-    var data, xhr, username, password;
+  xhr = new XMLHttpRequest();
 
-    method = (method || "GET").toUpperCase();
-    options = options || {};
-    options.contentType = options.contentType || "application/json; charset=utf-8";
-    username = _.isString(options.username) && options.username || "";
-    password = _.isString(options.password) && options.password || "";
+  return new Promise(function (resolve, reject) {
+    xhr.addEventListener("load", buildResolved(resolve));
+    xhr.addEventListener("error", buildRejected(reject));
+    xhr.addEventListener("abort", buildRejected(reject));
 
-    xhr = new XMLHttpRequest();
+    xhr.open(method, url, true, username, password);
 
-    return new Promise(function (resolve, reject) {
-      xhr.addEventListener("load", buildResolved(resolve));
-      xhr.addEventListener("error", buildRejected(reject));
-      xhr.addEventListener("abort", buildRejected(reject));
-
-      xhr.open(method, url, true, username, password);
-
-      if (method !== "GET") {
-        if (_.isObject(options.data)) {
-          data = JSON.stringify(options.data);
-        } else if (_.isString(options.data)) {
-          data = options.data;
-        } else {
-          data = "{}";
-        }
-
-        xhr.setRequestHeader("content-type", options.contentType);
+    if (method !== "GET") {
+      if (_.isObject(options.data)) {
+        data = JSON.stringify(options.data);
+      } else if (_.isString(options.data)) {
+        data = options.data;
+      } else {
+        data = "{}";
       }
 
-      xhr.send(data);
-    });
+      xhr.setRequestHeader("content-type", options.contentType);
+    }
 
-  };
+    xhr.send(data);
+  });
 
-  return {
-    get: _.partial(request, "GET"),
-    post: _.partial(request, "POST"),
-    put: _.partial(request, "PUT"),
-    request: request,
-    assertSuccess: assertSuccess
-  };
+};
 
-});
+module.exports = {
+  get: _.partial(request, "GET"),
+  post: _.partial(request, "POST"),
+  put: _.partial(request, "PUT"),
+  request: request,
+  assertSuccess: assertSuccess
+};
