8cff9cf9ec29c5128646d2e465e71d09a917a191
Dale Bustad <dale.bustad@formidablelabs.com>
2014-10-16 14:01:04 -0700

    add a click helper

--- spec/helpers/test.js
+++ spec/helpers/test.js
@@ -1,4 +1,10 @@
 module.exports = {
+  click: function (el) {
+    var evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent(
+      "click", true, true, window, 0, 0, 0 ,0 ,0, false, false, false, false, 0, null);
+    return !el.dispatchEvent(evt);
+  },
   captureExceptions: function (done, fn) {
     return function () {
       try {
