7e6c36dbb41f22ef101dcec8aef78c0aad8e1bb8
Dale Bustad <dale.bustad@formidablelabs.com>
2014-10-16 14:00:55 -0700

    capture exceptions now returns a function rather than immediately invoking

--- spec/helpers/test.js
+++ spec/helpers/test.js
@@ -1,10 +1,12 @@
 module.exports = {
   captureExceptions: function (done, fn) {
-    try {
-      fn();
-      done();
-    } catch (error) {
-      done(error);
-    }
+    return function () {
+      try {
+        fn();
+        done();
+      } catch (error) {
+        done(error);
+      }
+    };
   }
 };
