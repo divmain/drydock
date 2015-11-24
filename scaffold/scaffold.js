f645303a68aba09eac6e801cdd673b3ab12d5a29
Dale Bustad <dale@divmain.com>
2015-11-24 14:36:23 -0800

    Scaffold: Capture transactions and pass to skeleton writeMocks method.

--- scaffold/scaffold.js
+++ scaffold/scaffold.js
@@ -5,37 +5,73 @@ import minimist from "minimist";
 import Promise from "bluebird";
 
 import setupServer from "./setup-server";
+import writeMocks from "./write-mocks";
 import text from "../lib/text";
 
 
-const transactions = [];
 
 function printRow (a, b, c) {
   console.log("" + text(a).rightJustify(5) + text(b).rightJustify(7) + " " + c);
 }
 
+function trapFirstCtrlC () {
+  let firstCtrlC = true;
+  return new Promise(resolve => {
+    process.on("SIGINT", () => {
+      if (!firstCtrlC) {
+        console.log("exiting early...\n");
+        process.exit();
+      }
+      firstCtrlC = false;
+      resolve();
+    });
+  });
+}
+
 function main (options) {
   const {
     ip = "0.0.0.0",
-    port = 1337
+    port = 1337,
+    destination = process.cwd()
   } = options;
 
-  function onRequest({ method, href, transactionNo }) {
+  const transactions = [];
+
+  function onRequest({ method, hostname, pathname, href, transactionNo }) {
     printRow(transactionNo, text(method.toUpperCase()).yellow(), href);
+    transactions[transactionNo] = { method, hostname, pathname, href };
   }
 
   function onResponse({ statusCode, method, href, transactionNo, body, headers }) {
     const color = statusCode >= 200 && statusCode < 300 ? "green" : "red";
     printRow(transactionNo, text(method.toUpperCase())[color](), href);
-    fs.writeFileSync(path.join(__dirname, transactionNo.toString()), body);
+
+    Object.assign(transactions[transactionNo], {
+      statusCode,
+      responseBody: body,
+      responseHeaders: headers
+    });
   }
 
   const [ start, stop ] = setupServer({ ip, port}, onRequest, onResponse);
-  start()
-    .then(() => console.log("it is started!"))
-    .delay(10000)
-    .then(stop)
-    .then(() => console.log("it is finished!"));
+
+  Promise.resolve()
+    .then(() => {
+      console.log("starting server...");
+      return start();
+    })
+    .then(trapFirstCtrlC)
+    .then(() => {
+      console.log("\nstopping server...");
+      return stop();
+    })
+    .then(() => {
+      console.log("writing mocks to disk...")
+      return writeMocks(ip, port, destination, transactions);
+    })
+    .then(() => {
+      console.log("finished!");
+    });
 }
 
 main(minimist(process.argv.slice(2)));
---
+++ scaffold/write-mocks.js
@@ -0,0 +1,4 @@
+export default function writeMocks (ip, port, destination, transactions) {
+  // TODO: sort transactions by endpoint, enumerate, and write to disk using
+  //       AST templates
+}
