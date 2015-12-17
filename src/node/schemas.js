bf93e8467447320582990fc4cf7aaeba12e82af3
Dale Bustad <dale@divmain.com>
2015-12-16 20:13:53 -0800

    More permissive `path` pattern in route schema.
    Closes #2.

--- src/node/schemas.js
+++ src/node/schemas.js
@@ -20,7 +20,7 @@ export function validateApiUpdate (obj, schema) {
 export const route = Joi.object().keys({
   name: Joi.string().regex(/^[A-Za-z0-9\-]+$/).required(),
   method: Joi.string().allow("*", "GET", "POST", "PUT", "DELETE", "PATCH").required(),
-  path: Joi.string().regex(/^[A-Za-z\-\/\_\{\}]+$/).required(),
+  path: Joi.string().required(),
   handlers: Joi.object().required(),
   hostname: Joi.string().optional()
 });
